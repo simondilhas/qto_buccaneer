20e1c32c3785d9d368e8c9eec0c6a57ac5950f7f
simondilhas <simon.dilhas@abstract.build>
2025-04-08 12:24:03 +0200

    setup py update

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+import os
 
 # Read requirements from requirements.txt
 with open('requirements.txt') as f:
     requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
-
-
+# Import version from _version.py
+with open(os.path.join("src", "qto_buccaneer", "_version.py")) as f:
+    exec(f.read())
 
 setup(
     name="qto_buccaneer",
-    version=VERSION,
+    version=__version__,  # Use the imported version
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
